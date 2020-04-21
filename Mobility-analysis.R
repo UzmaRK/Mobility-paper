7050c64f2b386a6ee63e0bff00a9d7f1c0662133
UzmaRK <53031689+UzmaRK@users.noreply.github.com>
2020-04-21 05:35:22 +0500

    Add files via upload

---
+++ Mobility-analysis.R
@@ -0,0 +1,122 @@
+## Statistical analysis script
+##Independent mobility of adolescents and road traffic injuries
+## Authors:
+## Uzma Rahim Khan
+## Junaid Razzak
+## Martin Gerdin Wärnberg
+
+
+## Fixing the working director
+setwd("C:/Users/uzma.khan/Desktop/R-IM-URK")
+## Reading the data from the above mentioned directory
+data <- read.csv("Mobility-data.csv",header=TRUE)
+
+## Load required packages
+install.packages("data.table")
+library(data.table)
+install.packages("tableone")
+library(tableone)
+install.packages("survival")
+library(survival)
+install.packages("ISLR")
+library(ISLR)
+
+#data description
+#Description of attribute of the variable
+head(data)
+# Data observation and number of variable 
+dim(data)
+# Checking qualitative and quantitative variable
+str(data)
+
+### Get Variable names
+dput(names(data))
+
+
+  
+#Categories of Age groups of children
+
+data$agecat <- as.numeric(ifelse(data$Age < 13, 1,ifelse(data$Age >14, 3, 2)))
+
+data$agecat <- as.factor(data$agecat)
+
+levels(data$agecat) <- c("9 to 12","13 to 14","15 to 20")
+summary(data$agecat)
+
+## Vector of variable to summarize
+myVars <- c("agecat", "Age", "Gender", "Grade", "TYPEOFSC", "shift_school", 
+            "School.transport", "Transport_2or3wheelers", "Travel_Accompany", 
+            "Travel.accompany1", "Travel.accompany2", "TimeSchool", "time_school", 
+            "SchoolHomeTransport", "home_schooltransportusual", "Returned_Travel_Accompany", 
+            "Parents_Trust", "Cross_road", "Allow_Public_Bus", "RTI", "RTI_Consult", 
+            )
+catVar <- c("Grade")
+
+## Create Descriptive table
+tab1 <-CreateTableOne(vars = myVars,data = data,factorVar = catVar)
+tab1
+
+
+## Create Cross tabulation by taking outcome RTI
+tab2 <-CreateTableOne(vars = myVars, strata = "RTI", data = data, factorVars = catVar)
+tab2
+
+# Univariate Logistic regression of age groups with RTI
+
+
+model1=glm(data$RTI ~ relevel(data$agecat, ref = 1),  
+           family = "binomial", data=data)
+summary(model1)
+lreg.or <-exp(cbind(OR = coef(model3), confint(model3)))
+round(lreg.or, digits=2)
+
+# Logistic regression of public versus private school with RTI
+
+model2=glm(data$RTI ~ data$TYPEOFSC, family = "binomial")
+summary(model2)
+exp(cbind(OR = coef(model2), confint(model2)))
+
+
+# Logistic regression of Gender with RTI
+
+model5=glm(data$RTI ~ data$Gender,  family = "binomial")
+summary(model5)
+exp(cbind(OR = coef(model5), confint(model5)))
+
+# Logistic regression of Accompany to school with RTI
+
+model6=glm(RTI ~ relevel(Travel_Accompany, ref = 1),  
+           family = "binomial", data=data)
+summary(model6)
+exp(cbind(OR = coef(model6), confint(model6)))
+
+# Logistic regression of Returned_Travel_Accompany with RTI
+
+model7=glm(data$RTI ~ data$Returned_Travel_Accompany,  family = "binomial")
+summary(model7)
+exp(cbind(OR = coef(model7), confint(model7)))
+
+# Logistic regression of Parent Trust with RTI
+
+model8=glm(data$RTI ~ data$Parents_Trust,  family = "binomial")
+summary(model8)
+exp(cbind(OR = coef(model8), confint(model8)))
+
+# Logistic regression of Allow_Public_Bus with RTI
+
+model9=glm(data$RTI ~ data$Allow_Public_Bus,  family = "binomial")
+summary(model9)
+exp(cbind(OR = coef(model9), confint(model9)))
+
+
+# Logistic regression of CRoss main road with RTI
+
+model10=glm(data$RTI ~ data$Cross_road,  family = "binomial")
+summary(model10)
+exp(cbind(OR = coef(model10), confint(model10)))
+
+
+
+
+
+
